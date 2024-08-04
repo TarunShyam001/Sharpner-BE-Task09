f1513c0975ad1530dcf56b281c96cd223fe695ba
TarunShyam001 <shyamtarun2001@gmail.com>
2024-08-04 23:07:45 +0530

    Debugging is performed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const route = require('./routes');
 
 const server = http.createServer(route.handler);
 
-const PORT = 9200;
+const PORT = 7200;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
